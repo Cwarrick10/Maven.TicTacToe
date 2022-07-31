01c23e60fe8cc0b62ff5553572edd166ac795d91
Cwarrick10 <warrickchar@gmail.com>
2022-07-30 22:03:07 -0400

    done

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,53 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] board;
     public Board(Character[][] matrix) {
+        board = matrix;
+    }
+    private Boolean isInFavorOfChar(Character c) {
+        int[] turn = new int[8];
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col] == c) {
+                    turn[row]++;
+                    turn[col + 3]++;
+                    turn[7] = (row + col == 2) ? ++turn[7] : turn[7];
+                    turn[6] = (row == col) ? ++turn[6] : turn[6];
+                }
+            }
+        }
+        for (int i : turn) {
+            if (i == 3) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return isInFavorOfChar('X');
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        return isInFavorOfChar('O');
     }
 
     public Boolean isTie() {
-        return null;
+        return !(isInFavorOfX() || isInFavorOfO() );
     }
 
     public String getWinner() {
-        return null;
+        for(int i= 0; i < 3; i++) {
+            if (isInFavorOfX()) {
+                return "X";
+            }
+            if (isInFavorOfO()) {
+                return "O";
+            }
+        }
+
+        return "";
     }
 
 }
